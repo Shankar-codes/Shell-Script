a779a85655db7ba426d1d5ce8525af7ba74eb597
Shankar-codes <harikainteriors@gmail.com>
2026-05-02 18:32:56 +0530

    Variables

---
+++ 04-variables.sh
@@ -0,0 +1,6 @@
+person1=Trump
+person2=Modi
+
+echo "$person1 :: Hello $person2, Welcome to the event?"
+echo "$person2 :: Hi $person1, Thanks for inviting me."
+echo "$person1 :: I am doing good as well. Thanks for asking."
