f8175228a83a5a0901f52800ff6d520d5aacbbaa
Shankar-codes <harikainteriors@gmail.com>
2026-05-02 18:25:22 +0530

    conversion shell script

---
+++ 02-conversion.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Suresh:  Hi Ramesh, how are you?"
+echo "Ramesh:  Hi Suresh, I am fine. How are you?"
+echo "Suresh:  I am also fine. What are you doing these days?"
+echo "Ramesh:  I am learning DevOps. It is very interesting."
