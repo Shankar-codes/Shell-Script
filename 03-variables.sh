bf9b023c035d57ef2d88f179a830a9e8eadbbb34
Shankar-codes <harikainteriors@gmail.com>
2026-05-02 18:37:40 +0530

    commited

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-PERSON1= John
-PERSON2= Shankar
+PERSON1=John
+PERSON2=Shankar
 
 echo "$PERSON1 :: Hello $PERSON2, how are you doing?"
 echo "$PERSON2 :: Hi $PERSON1, I am doing good. How about you?"
---
+++ 05-variables.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Enter the PIN Number: "
+read pin
+echo "The PIN Number you entered is: $pin"
