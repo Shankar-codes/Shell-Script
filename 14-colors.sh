2b6684047917c95ff5664ea46680a9d4ab7e4f17
Shankar-codes <harikainteriors@gmail.com>
2026-05-03 13:24:32 +0530

    Colors

---
+++ 14-colors.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+echo -e "$R Hello World"
+echo -e "$G Shell Scripting"
+echo -e "$Y Shankar using yellow color to print the output"
+echo -e "$N This is normal text"
