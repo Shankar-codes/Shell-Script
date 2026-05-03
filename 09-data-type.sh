d27a90f888c523d830cedb8eb06377a292f3ff38
Shankar-codes <harikainteriors@gmail.com>
2026-05-03 11:13:14 +0530

    Data-type

---
+++ 09-data-type.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+NUMBER1=100
+NUMBER2=200
+NUMBER3=DevOps
+SUM=$((NUMBER1+NUMBER2+NUMBER3)) 
+#NUMBER3 is a string, so it will be treated as 0 in the arithmetic operation
+echo "The sum is $SUM"
