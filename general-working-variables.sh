452a5328640069915f2921a2ac496aac6e843236
Shankar-codes <harikainteriors@gmail.com>
2026-05-03 11:07:42 +0530

    general-working-variables.sh

---
+++ general-working-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "script name: $0"
+echo "Current working directory: $PWD"
+echo "Current user: $USER"
+echo "Home directory: $HOME"
+echo "PID of the script: $$"
+echo "Exit status of the last command: $?"
+sleep 5
