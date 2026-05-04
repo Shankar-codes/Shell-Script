6e7f0ddc6c6d940b3ee58c809dd0aa9821673ce4
Shankar-codes <harikainteriors@gmail.com>
2026-05-04 16:23:27 +0530

    Deleting the backlogs files older than 14 days

--- 22-delete-old-logs.sh
+++ 22-delete-old-logs.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
+
 LOGS_FOLDER="/var/log/Shell-Script"
-SCRIPT_NAME=$(echo "$0" | cut -d '.' -f1)
+SCRIPT_NAME=$(basename "$0" .sh)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
-mkdir -p LOGS_FOLDER
+# Create logs folder
+mkdir -p "$LOGS_FOLDER"
 
-echo "Script execution started at $(date)..." | tee -a &LOG_FILE
+echo "Script execution started at $(date)..." | tee -a "$LOG_FILE"
 
-SOURCE_DIR=/home/ec2-user/app-logs
+SOURCE_DIR="/home/ec2-user/app-logs"
 
-if [ ! -d $SOURCE_DIR ]; then
-	echo "ERROR :: $SOURCE_DIR does not exists"
-	exit 1;
+if [ ! -d "$SOURCE_DIR" ]; then
+    echo "ERROR :: $SOURCE_DIR does not exist" | tee -a "$LOG_FILE"
+    exit 1
 fi
 
-FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -type -f -mtime +14)
+FILES_TO_DELETE=$(find "$SOURCE_DIR" -name "*.log" -type f -mtime +14)
 
 while IFS=read -r filepath
 do
-	echo "Deleting the file: $filepath"
-	rm -rf $filepath
-	echo "Deleted the file:: $filepath"
-done <<< $FILES_TO_DELETE
+    echo "Deleting the file: $filepath" | tee -a "$LOG_FILE"
+    rm -rf "$filepath"
+    echo "Deleted the file: $filepath" | tee -a "$LOG_FILE"
+done <<< "$FILES_TO_DELETE"
